5949e1dbde530d103bbde7da922c7d3bca8379fb
le0nard01 <68928875+le0nard01@users.noreply.github.com>
2021-03-30 17:33:17 -0300

    update

--- sha1.py
+++ sha1.py
@@ -4,17 +4,63 @@ def encode(string,tipo='str'):
 
     def tobin(num): # transformar int em binario e preencher por 8, ou seja, tobin(10), 10 em binario é 1010, ele retornará 00001010.
         return bin(num)[2:].zfill(8)
-        
-    #constantes 
-    h0 = 0x67452301
-    h1 = 0xEFCDAB89
-    h2 = 0x98BADCFE
-    h3 = 0x10325476
-    h4 = 0xC3D2E1F0
+
+    def leftrotate(num, tam):
+        return ((num << tam) | (num >> (32 - tam))) % (2**32)
 
     def message_Schedule(data):
-        #
-    
+        if len(data) % 512 != 0:
+            print(f"Data não é particionada em 512. Tamanho: {len(data)}")
+            return 0
+
+        chunks = [ data[i:i+512] for i in range(0, len(data), 512) ] # Divide os chunks de 512
+
+        #constantes 
+        h0 = 0x67452301
+        h1 = 0xEFCDAB89
+        h2 = 0x98BADCFE
+        h3 = 0x10325476
+        h4 = 0xC3D2E1F0
+
+        for single_chunk in chunks:
+            (a,b,c,d,e) = (h0,h1,h2,h3,h4)
+
+            chunk32 = [ int(single_chunk[i:i+32],2) for i in range(0, len(single_chunk), 32) ] #Dividir em chunks de 32
+
+            for i in range(0,80-len(chunk32)): chunk32.append('0'*32) # acrescentar 0 para o total de len() = 32
+
+            for i in range(16,80):
+                chunk32[i] = leftrotate(chunk32[i-3] ^ chunk32[i-8] ^ chunk32[i-14] ^ chunk32[i-16], 1)
+
+            for i in range(80):
+                if 0 <= i <= 19 :
+                    f = ((b & c) | ((~b) & d)) % (2**32)
+                    k = 0x5A827999
+                elif 20 <= i <= 39 :
+                    f = (b ^ c ^ d) % (2**32)
+                    k = 0x6ED9EBA1
+                elif 40 <= i <= 59 :
+                    f = ((b & c) | (b & d) | (c & d) ) % (2**32)
+                    k = 0x8F1BBCDC
+                elif 60 <= i <= 79 :
+                    f = (b ^ c ^ d) % (2**32)
+                    k = 0xCA62C1D6
+
+                b = a
+                d = c
+                d = e
+                a = (leftrotate(a, 5) + f + e + k + chunk32[i]) % (2**32)
+                c = leftrotate(b, 30)
+
+            h0 = (h0 + a) % (2**32)
+            h1 = (h1 + b) % (2**32)
+            h2 = (h2 + c) % (2**32)
+            h3 = (h3 + d) % (2**32)
+            h4 = (h4 + e) % (2**32)
+
+        hashfinal = hex(h0),hex(h1),hex(h2),hex(h3),hex(h4)
+        return( ''.join([(i[2:]).zfill(8    ) for i in hashfinal]))
+
     if tipo=='str':
         bits = [ tobin(ord(x)) for x in string ] # iterar a string passada e encaminhar pra funcao tobin
 
@@ -34,3 +80,5 @@ def encode(string,tipo='str'):
     bits += '0'*(64-len(tobin(start_bits_len))) + tobin(start_bits_len) #adiciona o tamanho total no final dos bits e preenche.
 
     return message_Schedule(bits)
+
+print(encode('abc') == 'a9993e364706816aba3e25717850c26c9cd0d89d')
--- sha512.py
+++ sha512.py
@@ -22,7 +22,7 @@ def findroot_slowly_and_badly(n, k):
 initial_round_constants = [hex(findroot_slowly_and_badly(i,3))
                         for i in [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409]]
 
-initial_hash_values = [hex(int(modf(i**Decimal(1/2))[0] * (1<<64)))
+initial_hash_values = [hex(int(modf2(i**Decimal(1/2))[0] * (1<<64)))
                         for i in [2,3,5,7,11,13,17,19]]
 
 # PROCESSO PARA OBTER IV DE SHA512/t
@@ -44,7 +44,6 @@ def encode(string, tipo='str'):
         return bin(num)[2:].zfill(8)
 
     def rotate(num, tam):
-        #num = int(num,2)
         return ((num >> tam) | (num << (64 - tam))) % (2**64)
 
     def formathash(h): # formatar os hex, para todos hex ter 8 digitos incrementando 0's. E depois junta-los
