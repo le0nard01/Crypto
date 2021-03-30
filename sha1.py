c174227650f5c8e421948f738e72a4fd45ebe2ac
le0nard01 <68928875+le0nard01@users.noreply.github.com>
2021-03-30 16:49:57 -0300

    Update sha1.py

--- sha1.py
+++ sha1.py
@@ -1,8 +1,10 @@
-
-
-
+from math import ceil
 
 def encode(string,tipo='str'):
+
+    def tobin(num): # transformar int em binario e preencher por 8, ou seja, tobin(10), 10 em binario é 1010, ele retornará 00001010.
+        return bin(num)[2:].zfill(8)
+        
     #constantes 
     h0 = 0x67452301
     h1 = 0xEFCDAB89
@@ -24,11 +26,11 @@ def encode(string,tipo='str'):
     
     bits.append('1') # adiciona 1 no final
     bits = ''.join(bits)
-    
-    zeros_512 = ceil((len(bits)+128)/1024)*1024 - len(bits) - 128 # completa com '0', por multiplos de 512 em relacao ao que couber do tamanho da string, - 64.
-    
-    bits += zeros_1024*'0' 
 
-    bits += '0'*(128-len(tobin(start_bits_len))) + tobin(start_bits_len) #adiciona o tamanho total no final dos bits e preenche.
+    zeros_512 = ceil((len(bits)+64)/512)*512 - len(bits) - 64 # completa com '0', por multiplos de 512 em relacao ao que couber do tamanho da string, - 64.
+    
+    bits += zeros_512*'0' 
+
+    bits += '0'*(64-len(tobin(start_bits_len))) + tobin(start_bits_len) #adiciona o tamanho total no final dos bits e preenche.
 
     return message_Schedule(bits)
