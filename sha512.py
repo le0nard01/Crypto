f4c5439cf71b6178c6ac2ab183f3c8918a51bb89
le0nard01 <68928875+le0nard01@users.noreply.github.com>
2021-03-30 16:10:31 -0300

    Update sha512.py

--- sha512.py
+++ sha512.py
@@ -1,7 +1,7 @@
 from math import ceil,sqrt
 from decimal import Decimal
 
-def modf2(x):
+def modf(x):
     z= [i for i in str(x).split('.')][::-1]
     z[0] = '0.'+z[0]
     z = [Decimal(i) for i in z]
@@ -13,19 +13,16 @@ def findroot_slowly_and_badly(n, k):
     step = (guess >> 4) + 1     
     while step != 0:
         guess_step_pow_k = (guess + step) ** k
-
         if guess_step_pow_k > n:
             step = step >> 1
-
         else:
             guess = guess + step
-
     return (guess & ((2**64) - 1)) 
 
 initial_round_constants = [hex(findroot_slowly_and_badly(i,3))
                         for i in [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409]]
 
-initial_hash_values = [hex(int(modf2(i**Decimal(1/2))[0] * (1<<64)))
+initial_hash_values = [hex(int(modf(i**Decimal(1/2))[0] * (1<<64)))
                         for i in [2,3,5,7,11,13,17,19]]
 
 # PROCESSO PARA OBTER IV DE SHA512/t
